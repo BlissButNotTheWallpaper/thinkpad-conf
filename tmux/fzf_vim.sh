67aa2b61fb987f7de70381e80486141b81e17db5
BlissButNotTheWallpaper <fuckerfucker@tutamail.com>
2025-10-20 13:55:57 +0800

    changed hardcoded path

--- tmux/fzf_vim.sh
+++ tmux/fzf_vim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tmux new-window "bash -i -c 'FILE=\$(fdfind . "/home/niffer" --type file --hidden --max-depth 3 --exclude '**/.cache' --exclude '**/.mozilla' --exclude '**autostart' | fzf --preview \"cat {}\" --height 50%); if [ -n \"\$FILE\" ]; then cd \"\$(dname \"\$FILE\")\"; vim \"\$FILE\"; fi; exec bash'"
+tmux new-window "bash -i -c 'FILE=\$(fdfind . "$HOME" --type file --hidden --max-depth 3 --exclude '**/.cache' --exclude '**/.mozilla' --exclude '**autostart' | fzf --preview \"cat {}\" --height 50%); if [ -n \"\$FILE\" ]; then cd \"\$(dname \"\$FILE\")\"; vim \"\$FILE\"; fi; exec bash'"
 
